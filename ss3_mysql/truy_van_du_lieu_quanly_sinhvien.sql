c83bf9a0b25aafb757e61aee7864bf0ca9d5f343
TruongTheHieu <thehieu191099@gmail.com>
2024-01-09 11:58:45 +0700

    update

--- ss3_mysql/truy_van_du_lieu_quanly_sinhvien.sql
+++ ss3_mysql/truy_van_du_lieu_quanly_sinhvien.sql
@@ -35,36 +35,9 @@ CREATE TABLE Mark
     FOREIGN KEY (SubId) REFERENCES Subject (SubId),
     FOREIGN KEY (StudentId) REFERENCES Student (StudentId)
 );
-INSERT INTO Class
-VALUES (1, 'A1', '2008-12-20', 1);
-INSERT INTO Class
-VALUES (2, 'A2', '2008-12-22', 1);
-INSERT INTO Class
-VALUES (3, 'B3', current_date, 0);
-
-INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
-VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
-INSERT INTO Student (StudentName, Address, Status, ClassId)
-VALUES ('Hoa', 'Hai phong','0982737738', 1, 1);
-INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
-VALUES ('Manh', 'HCM', '0123123123', 0, 2);
-
-INSERT INTO Subject
-VALUES (1, 'CF', 5, 1),
-       (2, 'C', 6, 1),
-       (3, 'HDJ', 5, 1),
-       (4, 'RDBMS', 10, 1);
-       
-INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
-VALUES (1, 1, 8, 1),
-       (1, 2, 10, 2),
-       (2, 1, 12, 1);
-select * from class;
-select * from mark;
-select * from student;
-select * from subject;
 -- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
 select * from student where StudentName like  'h%';
+
 -- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
 -- c1:
  select * from class where StartDate like '%12%';
@@ -85,4 +58,4 @@ SET SQL_SAFE_UPDATES = 1; -- không khuyến khích dùng, vì việc này nguy 
 -- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
 select student.studentname, subject.subname, mark.mark
 from student,subject,mark
-order by mark.mark desc, student.studentname asc
+order by mark.mark desc
